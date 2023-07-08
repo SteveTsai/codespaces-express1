e5b1bb3d2c3b3e236348a6d7c4cfa832598ac620
SteveTsai <y721.tsai@gmail.com>
2023-07-08 15:39:24 +0000

    Send html file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello steve1!')
+  res.sendFile(path.join(__dirname, '/sss.html'))
 })
 
 app.get('/v1', (req, res) => {
