2323df172c001d315e6776cd7226b28ae7b7f51d
SteveTsai <y721.tsai@gmail.com>
2023-07-08 01:06:28 +0000

    Add v1 api

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello steve1!')
+})
+
+app.get('/v1', (req, res) => {
+  res.send('Hello v1!')
 })
 
 app.listen(port, () => {
