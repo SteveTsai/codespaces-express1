ab58d1ee3c3ccd86aeda13d7c49f337d35ddaf4c
steve-eatme <steve@eatme.tw>
2023-07-11 06:31:21 +0800

    Import Ben's full site

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/sss.html'))
+  res.sendFile(path.join(__dirname, '/home.html'))
 })
 
 app.get('/v1', (req, res) => {
